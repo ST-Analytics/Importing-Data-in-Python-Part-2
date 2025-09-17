ed889d184af576574a4d82e8962dab7dc535c447
ST-Analytics <151568912+ST-Analytics@users.noreply.github.com>
2025-09-17 10:57:48 +0530

    Add files via upload

---
+++ 2-interacting-with-apis-to-import-data-from-the-web/json-from the web.py
@@ -0,0 +1,19 @@
+#JSON–from the web to Python
+#However, as you know, your response is actually a JSON, so you can do one step better and decode the JSON.
+#Print the key-value pairs of the resulting dictionary. 
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
